fa4cdfff330f9a78b62f43396d01915258a387fa
Harsh  Gupta <harshgupta9175478116@gmail.com>
2023-11-28 13:40:12 +0530

    hi

---
+++ src/com/mkpits/Methods/StaticMethodCall.java
@@ -0,0 +1,24 @@
+package com.mkpits.Methods;
+
+public class StaticMethodCall {
+
+	public static void main(String[] args) {
+		//call by className.methodname
+		
+		System.out.println("This is calling by className.methodName");
+		StaticMethod.getFirstName();
+		StaticMethod.getLastName();
+		StaticMethod.getEmail();
+		StaticMethod.getGender();
+		StaticMethod.getPassword();
+		
+		
+		// call by objectref.methodname
+		System.out.println();
+		StaticMethod call = new StaticMethod();
+		System.out.println("This is calling by objectref.methodName");
+		call.getAllData();
+
+	}
+
+}
